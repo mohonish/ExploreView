06fab072986b782966d3f48c77cda2a1ecc879c6
Mohonish Chakraborty <mohonish.c@gmail.com>
2016-07-15 05:18:28 +0530

    added MCExploreAnimator

---
+++ ExploreView/Utility Classes/MCExploreAnimator.swift
@@ -0,0 +1,30 @@
+//
+//  MCExploreAnimator.swift
+//  ExploreView
+//
+//  Created by Mohonish Chakraborty on 15/07/16.
+//  Copyright © 2016 mohonish. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MCExploreAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    // MARK: - Class Members
+    
+    weak var transitionContext: UIViewControllerContextTransitioning?
+    
+    //MARK: - Duration
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        //TODO.
+        
+    }
+    
+}
