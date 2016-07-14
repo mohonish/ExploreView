03b24210a237c6949f4eab989fbfe6ada1571469
Mohonish Chakraborty <mohonish.c@gmail.com>
2016-07-15 00:39:43 +0530

    created featured cell

---
+++ ExploreView/Custom Views/FeaturedCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeaturedCollectionViewCell.swift
+//  ExploreView
+//
+//  Created by Mohonish Chakraborty on 15/07/16.
+//  Copyright © 2016 mohonish. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
